95d6da27753a09b5b424296ed8e6029006953ebe
unknown <gabrielgianotti@outlook.com>
2019-10-08 01:09:42 -0300

    add show image

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes.js');
 
@@ -14,6 +15,7 @@ mongoose.connect('mongodb+srv://suubmundo:NgUy7WzdgqmAuAJH@aircnc-tesgy.mongodb.
 //app.use(cors({origin: 'http:localhost:3000'}));
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(5000);
